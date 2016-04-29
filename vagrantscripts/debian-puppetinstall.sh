7cc1eb6ebe9753b6af642544bfd415425e45c795
Mans Matulewicz <cybermans@gmail.com>
2016-04-29 10:20:29 +0200

    cleanup

--- vagrantscripts/debian-puppetinstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-iptables --flush
-wget https://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
-dpkg -i puppetlabs-release-wheezy.deb
-apt-get update
-apt-get install -o Dpkg::Options::="--force-confold" -y puppet
--- vagrantscripts/debian-puppetmasterinstall.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get install -o Dpkg::Options::="--force-confold" -y puppetmaster-passenger  && apt-get -y -o Dpkg::Options::="--force-confold" install puppetmaster
