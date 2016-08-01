cc407b04a6606d2907d563acf5eca3c743ec40c1
Mans Matulewicz <cybermans@gmail.com>
2016-08-01 14:54:18 +0200

    better vagrant key permissions

--- scripts/10-vagrantuser.sh
+++ scripts/10-vagrantuser.sh
@@ -2,4 +2,5 @@
 mkdir ${rootfs}/home/vagrant/.ssh
 chmod 700 ${rootfs}/home/vagrant/.ssh
 curl -o ${rootfs}/home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub
-chown -R vagrant /home/vagrant/.ssh
+chown -R vagrant:vagrant /home/vagrant/.ssh
+chmod 600 /home/vagrant/.ssh/authorized_keys
