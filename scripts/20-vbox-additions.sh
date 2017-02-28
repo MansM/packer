c61b36a49236d040341688c988a29088a2ea1075
Mans Matulewicz <mans.matulewicz@gmail.com>
2017-02-28 11:12:32 +0100

    splitting kickstart config, seperate guestadditions

--- scripts/20-vbox-additions.sh
+++ scripts/20-vbox-additions.sh
@@ -1,3 +1,7 @@
+if [ ! -f  VBoxGuestAdditions.iso ] 
+  then 
+    curl -o  VBoxGuestAdditions.iso http://download.virtualbox.org/virtualbox/5.1.14/VBoxGuestAdditions_5.1.14.iso 
+fi
 mount -o loop VBoxGuestAdditions.iso /mnt
 yum install -y kernel-headers kernel-devel bzip2 gcc
 # Install the drivers
