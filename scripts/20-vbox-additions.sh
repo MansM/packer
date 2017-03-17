6b29dbe93ae4e7122a8c2a429147d59b159eb249
Mans Matulewicz <mans.matulewicz@gmail.com>
2017-03-17 13:54:20 +0100

    added changelog message and automated versioning of vbox guest additions

--- scripts/20-vbox-additions.sh
+++ scripts/20-vbox-additions.sh
@@ -1,6 +1,7 @@
 if [ ! -f  VBoxGuestAdditions.iso ] 
   then 
-    curl -o  VBoxGuestAdditions.iso http://download.virtualbox.org/virtualbox/5.1.14/VBoxGuestAdditions_5.1.14.iso 
+    vbox_version=$(curl http://download.virtualbox.org/virtualbox/LATEST.TXT)
+    curl -o  VBoxGuestAdditions.iso http://download.virtualbox.org/virtualbox/${vbox_version}/VBoxGuestAdditions_${vbox_version}.iso 
 fi
 mount -o loop VBoxGuestAdditions.iso /mnt
 yum install -y kernel-headers kernel-devel bzip2 gcc
